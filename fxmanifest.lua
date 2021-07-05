f436c0e59745dead8f736a39819dd75db28935f0
fsociety <72711660+cetvoropek@users.noreply.github.com>
2021-07-05 20:41:14 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'adamant'
+games { 'gta5' }
+
+author 'cetvoropek'
+description 'Fivem Loading Screen'
+version '1.0.0'
+
+loadscreen 'index.html'
+
+files {
+ 
+	'music.mp3',
+	'style.css',
+	'index.html',
+	'js/main.js',
+	'gif.gif',
+	'cursor.svg',
+    'js/plugins.js'
+}
